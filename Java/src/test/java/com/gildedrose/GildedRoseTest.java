6e3379874cdbbb53b6ec63df0657f485a5c4f376
gbovyn <gatien.bovyn@gmail.com>
2021-05-14 11:51:40 +0200

    test: Improve coverage

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,25 @@
 package com.gildedrose;
 
+import org.approvaltests.combinations.CombinationApprovals;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-class GildedRoseTest {
-
+class GildedRoseTest
+{
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void update_quality()
+    {
+        CombinationApprovals.verifyAllCombinations(this::doUpdateQuality,
+                new String[]{"foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros"},
+                new Integer[]{-1, 0, 3, 6, 11},
+                new Integer[]{0, 1, 50});
+    }
+
+    private Item doUpdateQuality(final String name, final int sellIn, final int quality)
+    {
+        Item[] items = new Item[]{new Item(name, sellIn, quality)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("foo", app.items[0].name);
+        return app.items[0];
     }
 
 }
