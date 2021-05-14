da1de55ed6c188ac17e62388a15c704764e8c4a9
gbovyn <gatien.bovyn@gmail.com>
2021-05-14 11:56:18 +0200

    refactor: Extract AgedBrie logic using lift-up refactoring

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -1,17 +1,45 @@
 package com.gildedrose;
 
-class GildedRose {
+class GildedRose
+{
     Item[] items;
 
-    public GildedRose(Item[] items) {
+    public GildedRose(Item[] items)
+    {
         this.items = items;
     }
 
-    public void updateQuality() {
+    public void updateQuality()
+    {
         for (final Item item : items)
         {
-            if (!item.name.equals("Aged Brie")
-                    && !item.name.equals("Backstage passes to a TAFKAL80ETC concert"))
+            doUpdate(item);
+        }
+    }
+
+    private void doUpdate(final Item item)
+    {
+        if (item.name.equals("Aged Brie"))
+        {
+            if (item.quality < 50)
+            {
+                item.quality = item.quality + 1;
+
+            }
+
+            item.sellIn = item.sellIn - 1;
+
+            if (item.sellIn < 0)
+            {
+                if (item.quality < 50)
+                {
+                    item.quality = item.quality + 1;
+                }
+            }
+        }
+        else
+        {
+            if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert"))
             {
                 if (item.quality > 0)
                 {
@@ -27,22 +55,19 @@ class GildedRose {
                 {
                     item.quality = item.quality + 1;
 
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert"))
+                    if (item.sellIn < 11)
                     {
-                        if (item.sellIn < 11)
+                        if (item.quality < 50)
                         {
-                            if (item.quality < 50)
-                            {
-                                item.quality = item.quality + 1;
-                            }
+                            item.quality = item.quality + 1;
                         }
+                    }
 
-                        if (item.sellIn < 6)
+                    if (item.sellIn < 6)
+                    {
+                        if (item.quality < 50)
                         {
-                            if (item.quality < 50)
-                            {
-                                item.quality = item.quality + 1;
-                            }
+                            item.quality = item.quality + 1;
                         }
                     }
                 }
@@ -55,31 +80,22 @@ class GildedRose {
 
             if (item.sellIn < 0)
             {
-                if (!item.name.equals("Aged Brie"))
+                if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert"))
                 {
-                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert"))
+                    if (item.quality > 0)
                     {
-                        if (item.quality > 0)
+                        if (!item.name.equals("Sulfuras, Hand of Ragnaros"))
                         {
-                            if (!item.name.equals("Sulfuras, Hand of Ragnaros"))
-                            {
-                                item.quality = item.quality - 1;
-                            }
+                            item.quality = item.quality - 1;
                         }
-                    }
-                    else
-                    {
-                        item.quality = 0;
                     }
                 }
                 else
                 {
-                    if (item.quality < 50)
-                    {
-                        item.quality = item.quality + 1;
-                    }
+                    item.quality = 0;
                 }
             }
         }
     }
+
 }
