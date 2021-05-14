debc9933f3f979809002a70fa277c29359a81c3c
gbovyn <gatien.bovyn@gmail.com>
2021-05-14 13:44:26 +0200

    refactor: Replace switch with map

--- Java/src/main/java/com/gildedrose/model/ItemFactory.java
+++ Java/src/main/java/com/gildedrose/model/ItemFactory.java
@@ -1,23 +1,51 @@
 package com.gildedrose.model;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
 public class ItemFactory
 {
+    private static final Map<String, Function<Item, BasicItem>> builder = new HashMap<>();
+
+    static
+    {
+        builder.put(
+                "Aged Brie",
+                ItemFactory::createAgedBrie
+        );
+        builder.put(
+                "Backstage passes to a TAFKAL80ETC concert",
+                ItemFactory::createBackstagePass
+        );
+        builder.put(
+                "Sulfuras, Hand of Ragnaros",
+                ItemFactory::createSulfuras
+        );
+    }
+
     public static BasicItem createItem(final Item item)
     {
-        final String name = item.name;
-        final int sellIn = item.sellIn;
-        final int quality = item.quality;
+        return builder.getOrDefault(item.name, ItemFactory::createBasicItem).apply(item);
+    }
 
-        switch (name)
-        {
-            case "Aged Brie":
-                return new AgedBrie(sellIn, quality);
-            case "Backstage passes to a TAFKAL80ETC concert":
-                return new BackstagePass(sellIn, quality);
-            case "Sulfuras, Hand of Ragnaros":
-                return new Sulfuras(sellIn, quality);
-            default:
-                return new BasicItem(name, sellIn, quality);
-        }
+    private static AgedBrie createAgedBrie(final Item item)
+    {
+        return new AgedBrie(item.sellIn, item.quality);
+    }
+
+    private static BackstagePass createBackstagePass(final Item item)
+    {
+        return new BackstagePass(item.sellIn, item.quality);
+    }
+
+    private static Sulfuras createSulfuras(final Item item)
+    {
+        return new Sulfuras(item.sellIn, item.quality);
+    }
+
+    private static BasicItem createBasicItem(final Item item)
+    {
+        return new BasicItem(item.name, item.sellIn, item.quality);
     }
 }
