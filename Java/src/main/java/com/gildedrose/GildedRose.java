d45edf066ac75894fef0108411f975ecefa971bb
gbovyn <gatien.bovyn@gmail.com>
2021-05-14 11:58:40 +0200

    refactor: Extract BackstagePass logic using lift-up refactoring

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -39,17 +39,7 @@ class GildedRose
         }
         else
         {
-            if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert"))
-            {
-                if (item.quality > 0)
-                {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros"))
-                    {
-                        item.quality = item.quality - 1;
-                    }
-                }
-            }
-            else
+            if (item.name.equals("Backstage passes to a TAFKAL80ETC concert"))
             {
                 if (item.quality < 50)
                 {
@@ -71,16 +61,30 @@ class GildedRose
                         }
                     }
                 }
-            }
 
-            if (!item.name.equals("Sulfuras, Hand of Ragnaros"))
-            {
                 item.sellIn = item.sellIn - 1;
-            }
 
-            if (item.sellIn < 0)
+                if (item.sellIn < 0)
+                {
+                    item.quality = 0;
+                }
+            }
+            else
             {
-                if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert"))
+                if (item.quality > 0)
+                {
+                    if (!item.name.equals("Sulfuras, Hand of Ragnaros"))
+                    {
+                        item.quality = item.quality - 1;
+                    }
+                }
+
+                if (!item.name.equals("Sulfuras, Hand of Ragnaros"))
+                {
+                    item.sellIn = item.sellIn - 1;
+                }
+
+                if (item.sellIn < 0)
                 {
                     if (item.quality > 0)
                     {
@@ -89,10 +93,6 @@ class GildedRose
                             item.quality = item.quality - 1;
                         }
                     }
-                }
-                else
-                {
-                    item.quality = 0;
                 }
             }
         }
